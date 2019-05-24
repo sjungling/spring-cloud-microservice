ec522261a4a2bcf3cedf34a23906a3c44adcea47
Allen <allenfedd@gmail.com>
2019-05-24 17:38:37 +0800

    add gateway service

---
+++ gateway-service/src/main/java/com/microweb/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.microweb.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+//@EnableEurekaClient //only Spring Cloud Netflix Eureka
+@EnableDiscoveryClient  //Spring Cloud Netflix Eureka, Spring Cloud Consul Discovery, Spring Cloud Zookeeper Discovery
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
